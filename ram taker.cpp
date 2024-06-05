c771c1d7f7b638f23eeb60d5ca1028ab538b83be
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-06-05 16:41:45 +0200

    Create ram taker.cpp
    kinda just a test to see if it takes up all the ram or if there is any optimising used

---
+++ ram taker.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	long long n;
+	cin>>n;
+	short tablica[n/2];
+	int i;
+	for(i=0;i<n/2;++i){
+		tablica[i] = 245;
+	}
+	while(true) cout<<1;
+	return 0;
+}
